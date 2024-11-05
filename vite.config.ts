dbfa8fa9ca95ad636db3b4718b9866fcf8635b59
Febryan Hernanda Mashudi <febryanhernanda@gmail.com>
2024-11-05 15:50:40 +0800

    renew path base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FebryanHernanda.github.io/",
+  base: "//",
   css: {
     postcss: {
       plugins: [tailwindcss()],
