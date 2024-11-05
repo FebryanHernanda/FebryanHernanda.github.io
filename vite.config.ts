071f4805acc191b9965b49208298b28892068547
Febryan Hernanda Mashudi <febryanhernanda@gmail.com>
2024-11-05 14:54:29 +0800

    adding config for deploy this apps

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/FebryanHernanda.github.io/",
   css: {
     postcss: {
       plugins: [tailwindcss()],
