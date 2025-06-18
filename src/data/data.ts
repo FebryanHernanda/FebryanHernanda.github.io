386e15e14235715147ede74cc0372c14c855c33b
Febryan Hernanda <febryanhernanda@Febryans-MacBook-Pro.local>
2025-06-18 08:15:40 +0700

    fix path image icon

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,94 +1,94 @@
 const techStackIcon = [
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/javascript.png",
+    imgSrc: "img/Icon/Tech-Stack/javascript.png",
     altImg: "Javascript Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/typescript.png",
+    imgSrc: "img/Icon/Tech-Stack/typescript.png",
     altImg: "Typescript Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/nodejs.png",
+    imgSrc: "img/Icon/Tech-Stack/nodejs.png",
     altImg: "NodeJs Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/reactjs.png",
+    imgSrc: "img/Icon/Tech-Stack/reactjs.png",
     altImg: "ReactJs Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/nextjs.png",
+    imgSrc: "img/Icon/Tech-Stack/nextjs.png",
     altImg: "NextJs Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/redux.png",
+    imgSrc: "img/Icon/Tech-Stack/redux.png",
     altImg: "Redux Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/mongodb.png",
+    imgSrc: "img/Icon/Tech-Stack/mongodb.png",
     altImg: "MongoDB Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/supabase.png",
+    imgSrc: "img/Icon/Tech-Stack/supabase.png",
     altImg: "Supabase Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/mysql.png",
+    imgSrc: "img/Icon/Tech-Stack/mysql.png",
     altImg: "MySQL Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/git.png",
+    imgSrc: "img/Icon/Tech-Stack/git.png",
     altImg: "Git Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/github.png",
+    imgSrc: "img/Icon/Tech-Stack/github.png",
     altImg: "Github Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/vite.png",
+    imgSrc: "img/Icon/Tech-Stack/vite.png",
     altImg: "Vite Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/html5.png",
+    imgSrc: "img/Icon/Tech-Stack/html5.png",
     altImg: "HTML5 Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/css.png",
+    imgSrc: "img/Icon/Tech-Stack/css.png",
     altImg: "CSS Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/tailwind.png",
+    imgSrc: "img/Icon/Tech-Stack/tailwind.png",
     altImg: "TailwindCSS Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/bootstrap.png",
+    imgSrc: "img/Icon/Tech-Stack/bootstrap.png",
     altImg: "Bootstrap Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/materialui.png",
+    imgSrc: "img/Icon/Tech-Stack/materialui.png",
     altImg: "MaterialUI Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/shadcnui.png",
+    imgSrc: "img/Icon/Tech-Stack/shadcnui.png",
     altImg: "ShadcnUI Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/figma.png",
+    imgSrc: "img/Icon/Tech-Stack/figma.png",
     altImg: "Figma Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/adobexd.png",
+    imgSrc: "img/Icon/Tech-Stack/adobexd.png",
     altImg: "Adobe Xd Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/photoshop.png",
+    imgSrc: "img/Icon/Tech-Stack/photoshop.png",
     altImg: "Adobe Photoshop Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/jira.png",
+    imgSrc: "img/Icon/Tech-Stack/jira.png",
     altImg: "Jira Icon",
   },
   {
-    imgSrc: "/public/img/Icon/Tech-Stack/wordpress.png",
+    imgSrc: "img/Icon/Tech-Stack/wordpress.png",
     altImg: "Wordpress Icon",
   },
 ];
