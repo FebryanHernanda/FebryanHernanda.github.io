a88c44a9ef528ea9accd79b8a74e14ac5f0e3d2f
FebryanHernanda <febryanhernanda@gmail.com>
2026-01-17 15:01:50 +0700

    style: Update `fullNameStyle` in FooterContainer config to use `lg:flex gap-1` instead of `lg:block`.

--- src/assets/container/FooterContainer/FooterContainer.config.ts
+++ src/assets/container/FooterContainer/FooterContainer.config.ts
@@ -2,7 +2,7 @@ const styleConfig = {
   container:
     "w-full flex flex-wrap justify-between mt-5 gap-10 p-5 lg:py-3 lg:flex-nowrap",
   imgStyle: "w-[50px] lg:w-[60px]",
-  fullNameStyle: "text-sm hidden lg:block",
+  fullNameStyle: "text-sm hidden lg:flex gap-1",
   fullNameSmallDevice: "lg:hidden",
   jobTitle: "hidden lg:block",
   leftWrapper:
