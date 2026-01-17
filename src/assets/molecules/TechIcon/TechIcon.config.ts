bc71b6fe08fa870fb6ae7a05d0f599078058ff71
FebryanHernanda <febryanhernanda@gmail.com>
2026-01-17 14:36:48 +0700

    style: add vertical padding to TechIcon left container.

--- src/assets/molecules/TechIcon/TechIcon.config.ts
+++ src/assets/molecules/TechIcon/TechIcon.config.ts
@@ -1,7 +1,7 @@
 const styleConfig = {
   wrapper: "flex flex-col items-center justify-center overflow-hidden gap-7",
   leftContainer:
-    "flex items-center justify-center gap-10 pl-[800px] md:pl-[400px] whitespace-nowrap opacity-90 ",
+    "flex items-center justify-center gap-10 pl-[800px] md:pl-[400px] whitespace-nowrap opacity-90 py-1",
   rightContainer:
     "flex items-center justify-center gap-10 pr-[800px] md:pr-[400px] whitespace-nowrap opacity-90",
   iconStyle: "w-10 lg:w-[3.3rem] max-w-none cursor-pointer hover:scale-110 transition-transform duration-200",
